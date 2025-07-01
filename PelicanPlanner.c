2168f5427042c053f68ec3cf14c9d44c0d5c10d9
= <=>
2025-07-01 16:15:49 -0300

    Main Menu Added

--- PelicanPlanner.c
+++ PelicanPlanner.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 typedef struct{
 
@@ -14,7 +16,29 @@ typedef struct{
 
 }Gift_List;
 
+void file_reader();
+void list_all_gifts();
+void search_gift();
+void main_menu();
+
 Gift_List gifts_List[50];
+
+
+
+int main(){
+
+    char archive[] = "gifts.csv";
+    file_reader(archive);
+    system("clear");
+    sleep(3);
+
+    main_menu();
+
+
+
+    return 0;
+
+}
 
 void file_reader(char archive[]){
 
@@ -42,53 +66,135 @@ void file_reader(char archive[]){
                 gifts_List[i].gift_4,
                 gifts_List[i].gift_5) == 6) {
             i++;
-        } else {
+        } 
+        else if(sscanf(line, "%[^,]", gifts_List[i].character) == 1){i++;}
+        
+        
+        else {
             printf("Erro ao processar linha: %s", line);
         }
     }
 
     printf("O arquivo foi lido com sucesso!!");
+
     fclose(file);
+
+}
+
+void list_all_gifts(){
+
+    int i = 0;
+    while(i < 50 && strcmp(gifts_List[i].character, "") != 0){
+
+        if(strcmp(gifts_List[i].gift_1, "") == 0){
+
+        printf("\n\n--------------------------------------------------");
+        printf("\n\nName of the character:      %s\n", gifts_List[i].character);
+        printf("This character can't receive gifts!");
+
+        printf("\n\n--------------------------------------------------\n\n");
+
+
+        }
+        else{
+        printf("\n\n--------------------------------------------------");
+        printf("\n\nName of the character:      %s\n", gifts_List[i].character);
+        printf("Gift 1:                     %s\n",gifts_List[i].gift_1);
+        printf("Gift 2:                     %s\n",gifts_List[i].gift_2);
+        printf("Gift 3:                     %s\n",gifts_List[i].gift_3);
+        printf("Gift 4:                     %s\n",gifts_List[i].gift_4);
+        printf("Gift 5:                     %s",gifts_List[i].gift_5);
+        printf("\n\n--------------------------------------------------\n\n");
+        }
+        i++;
+
+    }
+
 
 }
 
 void search_gift(){
 
     char name[30];
-    printf("\n\nInsert the character name: ");
-    scanf("%[^\n]", name);
+    printf("\nInsert the character name: ");
+    scanf(" %[^\n]", name);
+
+
 
     int i = 0;
 
     while (i < 50 && strcmp(gifts_List[i].character, name) != 0) {
 
-        if (i == 50) {
-            printf("Unable to find the character!\n");
-            return;
-        }
-
         i++;
 
     }
+
+    if(strcmp(gifts_List[i].character, "") == 0){
+
+        printf("The choosen character is either misspelled or he can't receive gifts!");
+        
+    }
+    else{
     printf("\n\n--------------------------------------------------");
-    printf("\n\nName of the character:      %s\n", gifts_List[i].character);
+    printf("\n\nName of the character:      %s\n\n", gifts_List[i].character);
     printf("Gift 1:                     %s\n",gifts_List[i].gift_1);
     printf("Gift 2:                     %s\n",gifts_List[i].gift_2);
     printf("Gift 3:                     %s\n",gifts_List[i].gift_3);
     printf("Gift 4:                     %s\n",gifts_List[i].gift_4);
     printf("Gift 5:                     %s\n",gifts_List[i].gift_5);
     printf("\n\n--------------------------------------------------\n\n");
-
+    }
 
 }
 
-int main(){
 
-    char archive[] = "gifts.csv";
-    file_reader(archive);
 
-    search_gift();
 
-    return 0;
+void main_menu(){
+
+    int option = 0;
+
+
+
+
+
+    while(option != 9){
+
+        printf("\n\n1. Select the character: \n");
+
+        printf("2. Show all characters: \n");
+
+        printf("3. Profit Calculator: \n");
+
+        printf("4. Tips: \n");
+
+        printf("5. Item Codes: \n\n");
+
+        printf("Insert the option: ");
+    
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+
+            search_gift();
+
+            break;
+        
+        case 2:
+
+            list_all_gifts();
+
+            break;
+
+        case 9:
+
+            break;
+
+
+        }
+
+    }
 
 }
